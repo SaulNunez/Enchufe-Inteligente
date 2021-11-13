08e56450904200fc79f77f997c67594a3022bf37
Saul Nuñez <saul.nunez99@gmail.com>
2021-11-13 00:21:38 -0700

    Formatear código

--- src/main.cpp
+++ src/main.cpp
@@ -10,96 +10,108 @@ AsyncWebServer server(80);
 
 fauxmoESP fauxmo;
 
-enum operationMode {
+enum operationMode
+{
   askWifiCredentials,
   justPlug
 };
 
-void setupWifiAp(){
+void setupWifiAp()
+{
   WiFi.mode(WIFI_AP_STA);
   WiFi.softAP("IntelliPlug1");
   Serial.println("AP address:" + WiFi.softAPIP().toString());
-
 }
 
 // Setup code necesary for setup mode
 // We need to enable stuff to discover networks before creating a soft AP
 // In which the user can give their network credential before normal usage
-void wifiDeviceSetupSetup(){
+void wifiDeviceSetupSetup()
+{
   setupWifiAp();
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/wifi_info.html", String(), false);
-  });
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+            { request->send(LittleFS, "/wifi_info.html", String(), false); });
 
-  server.on("/available_networks", HTTP_GET, [](AsyncWebServerRequest *request){
-    String returnObj = "{ \"wifiNetworks\": [";
-    int networksFound = WiFi.scanNetworks();
-    Serial.println("Networks found: " + networksFound);
-    for(int i = 0; i < networksFound; i++){
-      returnObj.concat("\"" + WiFi.SSID(i) + "\"");
-      if(i != networksFound - 1){
-        returnObj.concat(",");
-      }
-    }
-    returnObj.concat("] }");
-    Serial.println(returnObj);
+  server.on("/available_networks", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              String returnObj = "{ \"wifiNetworks\": [";
+              int networksFound = WiFi.scanNetworks();
+              Serial.println("Networks found: " + networksFound);
+              for (int i = 0; i < networksFound; i++)
+              {
+                returnObj.concat("\"" + WiFi.SSID(i) + "\"");
+                if (i != networksFound - 1)
+                {
+                  returnObj.concat(",");
+                }
+              }
+              returnObj.concat("] }");
+              Serial.println(returnObj);
 
-    request->send(200, "application/json", returnObj);
-  });
+              request->send(200, "application/json", returnObj);
+            });
 
-  server.on("/wifi_info", HTTP_POST, [](AsyncWebServerRequest *request){
-    String ssid;
-    String password;
+  server.on("/wifi_info", HTTP_POST, [](AsyncWebServerRequest *request)
+            {
+              String ssid;
+              String password;
 
-    if(request->hasParam("ssid", true)){
-      AsyncWebParameter* ssidParam = request->getParam("ssid", true);
-      ssid = ssidParam->value();
-    } else {
-      request->send(400, "text/plain", "SSID parameter not found");
-      return;
-    }
+              if (request->hasParam("ssid", true))
+              {
+                AsyncWebParameter *ssidParam = request->getParam("ssid", true);
+                ssid = ssidParam->value();
+              }
+              else
+              {
+                request->send(400, "text/plain", "SSID parameter not found");
+                return;
+              }
 
-    if(request->hasParam("password", true)){
-      AsyncWebParameter* passwordParam = request->getParam("password", true);
-      password = passwordParam->value();
-    } else {
-      request->send(400, "text/plain", "Password parameter not found");
-      return;
-    }
+              if (request->hasParam("password", true))
+              {
+                AsyncWebParameter *passwordParam = request->getParam("password", true);
+                password = passwordParam->value();
+              }
+              else
+              {
+                request->send(400, "text/plain", "Password parameter not found");
+                return;
+              }
 
-    Serial.println(("ssid:" + ssid + "\n" + "password:" + password).c_str());
+              Serial.println(("ssid:" + ssid + "\n" + "password:" + password).c_str());
 
-    File f = LittleFS.open("/wifi.conf", "w");
-    if(!f){
-      request->send(500);
-      return;
-    }
-    Serial.println("Saving connection parameters...");
-    f.write(("ssid:" + ssid + "\n" + "password:" + password).c_str());
-    f.close();
-    request->send(200);
-    ESP.restart();
-  });
+              File f = LittleFS.open("/wifi.conf", "w");
+              if (!f)
+              {
+                request->send(500);
+                return;
+              }
+              Serial.println("Saving connection parameters...");
+              f.write(("ssid:" + ssid + "\n" + "password:" + password).c_str());
+              f.close();
+              request->send(200);
+              ESP.restart();
+            });
   server.begin();
   Serial.println("HTTP server started");
 }
 
-const char* enchufeUno = "Enchufe Uno";
-const char* enchufeDos = "Enchufe Dos";
+const char *enchufeUno = "Enchufe Uno";
+const char *enchufeDos = "Enchufe Dos";
 
 #ifdef ESP32
-  #define RELAY_ENCHUFE_1 D2
-  #define RELAY_ENCHUFE_2 D4
-  #define TRIGGER_SETUP_MODE D13
-  #define TOGGLE_RELAY_1 D18
-  #define TOGGLE_RELAY_2 D19
+#define RELAY_ENCHUFE_1 D2
+#define RELAY_ENCHUFE_2 D4
+#define TRIGGER_SETUP_MODE D13
+#define TOGGLE_RELAY_1 D18
+#define TOGGLE_RELAY_2 D19
 #else
-  #define RELAY_ENCHUFE_1 D1
-  #define RELAY_ENCHUFE_2 D2
-  #define TRIGGER_SETUP_MODE D5
-  #define TOGGLE_RELAY_1 D6
-  #define TOGGLE_RELAY_2 D7
+#define RELAY_ENCHUFE_1 D1
+#define RELAY_ENCHUFE_2 D2
+#define TRIGGER_SETUP_MODE D5
+#define TOGGLE_RELAY_1 D6
+#define TOGGLE_RELAY_2 D7
 #endif
 
 EasyButton relayToggle1(TOGGLE_RELAY_1);
@@ -107,7 +119,8 @@ EasyButton relayToggle2(TOGGLE_RELAY_2);
 
 EasyButton setupModeButton(TRIGGER_SETUP_MODE);
 
-void setupWifi(){
+void setupWifi()
+{
   //Encender wifi
   WiFi.mode(WIFI_STA);
 
@@ -117,7 +130,7 @@ void setupWifi(){
 
   int newLineSeparator = wifiFileContents.indexOf('\n');
   String ssidFull = wifiFileContents.substring(0, newLineSeparator);
-  String passwordFull = wifiFileContents.substring(newLineSeparator+1);
+  String passwordFull = wifiFileContents.substring(newLineSeparator + 1);
 
   String ssid = ssidFull.substring(5);
   String password = passwordFull.substring(9);
@@ -139,9 +152,10 @@ void setupWifi(){
 bool relayState1 = false;
 bool relayState2 = false;
 
-void normalOperationSetup(){
+void normalOperationSetup()
+{
   Serial.println("IoT mode");
-  
+
   pinMode(RELAY_ENCHUFE_1, OUTPUT);
   pinMode(RELAY_ENCHUFE_2, OUTPUT);
   digitalWrite(RELAY_ENCHUFE_1, LOW);
@@ -153,15 +167,18 @@ void normalOperationSetup(){
   setupWifi();
 
   // These two callbacks are required for gen1 and gen3 compatibility
-  server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total) {
-      if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), String((char *)data))) return;
-      // Handle any other body request here...
-  });
-  server.onNotFound([](AsyncWebServerRequest *request) {
+  server.onRequestBody([](AsyncWebServerRequest *request, uint8_t *data, size_t len, size_t index, size_t total)
+    {
+      if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), String((char *)data)))
+        return;
+    });
+  server.onNotFound([](AsyncWebServerRequest *request)
+    {
       String body = (request->hasParam("body", true)) ? request->getParam("body", true)->value() : String();
-      if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), body)) return;
-      // Handle not found request here...
-  });
+      if (fauxmo.process(request->client(), request->method() == HTTP_GET, request->url(), body))
+        return;
+        // Handle not found request here...
+    });
 
   server.begin();
 
@@ -172,32 +189,41 @@ void normalOperationSetup(){
   fauxmo.addDevice(enchufeUno);
   fauxmo.addDevice(enchufeDos);
 
-  fauxmo.onSetState([](unsigned char device_id, const char * device_name, bool state, unsigned char value) {
-        Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
-        if(strcmp(device_name,enchufeUno) == 0){
-          relayState1 = state? HIGH: LOW;
-          digitalWrite(RELAY_ENCHUFE_1, relayState1);
-        } else if(strcmp(device_name, enchufeDos) == 0){
-          relayState2 = state? HIGH: LOW;
-          digitalWrite(RELAY_ENCHUFE_2, relayState2);
-        }
-    });
+  fauxmo.onSetState([](unsigned char device_id, const char *device_name, bool state, unsigned char value)
+    {
+      Serial.printf("[MAIN] Device #%d (%s) state: %s value: %d\n", device_id, device_name, state ? "ON" : "OFF", value);
+      if (strcmp(device_name, enchufeUno) == 0)
+      {
+        relayState1 = state ? HIGH : LOW;
+        digitalWrite(RELAY_ENCHUFE_1, relayState1);
+      }
+      else if (strcmp(device_name, enchufeDos) == 0)
+      {
+        relayState2 = state ? HIGH : LOW;
+        digitalWrite(RELAY_ENCHUFE_2, relayState2);
+      }
+  });
 }
 
 operationMode mode;
 
-void setup() {
-Serial.begin(115200);
-Serial.setDebugOutput(true);
-if(!LittleFS.begin()){
+void setup()
+{
+  Serial.begin(115200);
+  Serial.setDebugOutput(true);
+  if (!LittleFS.begin())
+  {
     Serial.println("An Error has occurred while mounting LittleFS");
     return;
   }
 
-  if(LittleFS.exists("/wifi.conf")){
+  if (LittleFS.exists("/wifi.conf"))
+  {
     mode = justPlug;
     normalOperationSetup();
-  } else {
+  }
+  else
+  {
     mode = askWifiCredentials;
     wifiDeviceSetupSetup();
   }
@@ -205,21 +231,27 @@ if(!LittleFS.begin()){
 
 const int FIVE_SECONDS = 5 * 1000;
 
-void loop() {
-  if(mode == justPlug){
+void loop()
+{
+  if (mode == justPlug)
+  {
     fauxmo.handle();
 
-    if(relayToggle1.wasPressed()){
+    if (relayToggle1.wasPressed())
+    {
       relayState1 = !relayState1;
       fauxmo.setState(enchufeUno, relayState1, 255);
     }
-    if(relayToggle2.wasPressed()){
+    if (relayToggle2.wasPressed())
+    {
       relayState2 = !relayState2;
       fauxmo.setState(enchufeDos, relayState2, 255);
     }
 
-    if(setupModeButton.pressedFor(FIVE_SECONDS)){
-      if(!LittleFS.remove("/wifi.conf")){
+    if (setupModeButton.pressedFor(FIVE_SECONDS))
+    {
+      if (!LittleFS.remove("/wifi.conf"))
+      {
         Serial.println("Something happened and network information could not be deleted!");
       }
       ESP.restart();
